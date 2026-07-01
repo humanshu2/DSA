7340530e2d2721ddddad33e3b65765e0fc815323
himanshukanyal <kanyalh7@gmail.com>
2026-07-01 19:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (49.48%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
-        while(n>1){
-            if(n%2!=0)return false;
-            n=n/2;
-        }
+     if((n & (n-1)) == 0) {
         return true;
+     }
+     else return false;     
     }
 };
