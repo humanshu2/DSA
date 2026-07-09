1a85611a754bc2ebe484b35e137d334eb73f2fbc
himanshukanyal <kanyalh7@gmail.com>
2026-07-09 18:43:24 +0530

    Time: 4 ms (44.49%) | Memory: 32.5 MB (98.68%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int atMost(vector<int>& nums, int goal) {
-        if(goal<0) return 0;
+    int atMost(vector<int>& nums,int goal){
         int left=0;
-        int cnt=0;
+        int right=0;
         int sum=0;
         int n=nums.size();
-        
-        for(int right=0;right<n;right++){
+        if(goal<0) return 0;
+        int cnt=0;
+        while(right<n){
             sum+=nums[right];
             while(sum>goal){
                 sum=sum-nums[left];
                 left++;
             }
-            cnt+=(right-left+1);
+            cnt=cnt+(right-left+1);
+            right++;
         }
         return cnt;
     }
-     int numSubarraysWithSum(vector<int>& nums,int goal){
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
         return atMost(nums,goal)-atMost(nums,goal-1);
     }
 };
